066ccf0aebfffea11763e082270bc896baf0af27
Miguel López <miguelopezv@gmail.com>
2026-04-02 01:00:58 -0500

    update components

---
+++ app/components/UI/CategoryIcon.tsx
@@ -0,0 +1,24 @@
+import { Category } from "@/src/generated/prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+type CategoryIconProps = { category: Category };
+
+export default function CategoryIcon({ category }: CategoryIconProps) {
+  return (
+    <div
+      className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
+    >
+      <div className="relative size-16">
+        <Image
+          src={`/icon_${category.slug}.svg`}
+          alt={`${category.name} icon`}
+          fill
+        />
+      </div>
+      <Link className="text-lg font-bold" href={`/order/${category.slug}`}>
+        {category.name}
+      </Link>
+    </div>
+  );
+}
--- app/components/order/OrderSidebar.tsx
+++ app/components/order/OrderSidebar.tsx
@@ -1,4 +1,5 @@
 import prisma from "@/src/lib/prisma";
+import CategoryIcon from "../UI/CategoryIcon";
 
 async function getCategories() {
   return await prisma.category.findMany();
@@ -6,6 +7,13 @@ async function getCategories() {
 
 export default async function OrderSidebar() {
   const categories = await getCategories();
-  console.log("🚀 ~ OrderSidebar ~ categories:", categories);
-  return <aside className="md:w-72 md:h-screen bg-white">OrderSidebar</aside>;
+  return (
+    <aside className="md:w-72 md:h-screen bg-white">
+      <nav className="mt-10">
+        {categories.map((category) => (
+          <CategoryIcon key={category.id} category={category} />
+        ))}
+      </nav>
+    </aside>
+  );
 }
