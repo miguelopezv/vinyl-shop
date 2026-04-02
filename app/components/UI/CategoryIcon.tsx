bb29c5063db0712cd91783abb24d6c7f413450fa
Miguel López <miguelopezv@gmail.com>
2026-04-02 12:09:15 -0500

    hide genre icons

--- app/components/UI/CategoryIcon.tsx
+++ app/components/UI/CategoryIcon.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Category } from "@/src/generated/prisma/client";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
@@ -14,13 +14,14 @@ export default function CategoryIcon({ category }: CategoryIconProps) {
     <div
       className={`${category.slug === params.category ? "bg-amber-400" : ""} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
     >
-      <div className="relative size-16">
+      {/*TODO: get some svg icons for genres*/}
+      {/* <div className="relative size-16">
         <Image
           src={`/icon_${category.slug}.svg`}
           alt={`${category.name} icon`}
           fill
         />
-      </div>
+      </div> */}
       <Link className="text-lg font-bold" href={`/order/${category.slug}`}>
         {category.name}
       </Link>
