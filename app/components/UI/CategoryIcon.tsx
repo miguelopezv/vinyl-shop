41392bab8b992c084f5fb91a7a891b079b0723ce
Miguel López <miguelopezv@gmail.com>
2026-04-02 12:07:23 -0500

    highlight current category

--- app/components/UI/CategoryIcon.tsx
+++ app/components/UI/CategoryIcon.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import { Category } from "@/src/generated/prisma/client";
 import Image from "next/image";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 type CategoryIconProps = { category: Category };
 
 export default function CategoryIcon({ category }: CategoryIconProps) {
+  const params = useParams<{ category: string }>();
+
   return (
     <div
-      className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
+      className={`${category.slug === params.category ? "bg-amber-400" : ""} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
     >
       <div className="relative size-16">
         <Image
