21d63608583ae2ab6fecb1bf55958ea3a05b0609
Miguel López <miguelopezv@gmail.com>
2026-04-02 14:18:10 -0500

    add constants to utils

---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const MIN_ITEMS = 1;
+export const MAX_ITEMS = 5;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
 import { formatCurrency } from "./formattters";
+import { MAX_ITEMS, MIN_ITEMS } from "./constants";
 
 export { formatCurrency };
+export { MAX_ITEMS, MIN_ITEMS };
