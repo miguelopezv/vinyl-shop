82428f2ba90d5e236a2cb86a7ae6bb0bb104296c
Miguel López <miguelopezv@gmail.com>
2026-04-02 12:07:33 -0500

    add title

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -15,6 +15,7 @@ export default async function OrderPage({
   const products = await getProducts(params.category);
   return (
     <>
+      <h1 className="text-2xl my-10">Select your products</h1>
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
