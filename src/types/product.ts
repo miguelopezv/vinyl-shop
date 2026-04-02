819bd9704d29ad303796123750061f8b90cf465c
Miguel López <miguelopezv@gmail.com>
2026-04-02 14:23:14 -0500

    add artist to orderItem type

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,6 +1,6 @@
 import { Product } from "@/src/generated/prisma/client";
 
-export type OrderItem = Pick<Product, "id" | "name" | "price"> & {
+export type OrderItem = Pick<Product, "id" | "name" | "artist" | "price"> & {
   quantity: number;
   subtotal: number;
 };
