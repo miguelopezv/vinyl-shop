224861255dc194b7cf3caeb534b4d17bdbdcee8f
Miguel López <miguelopezv@gmail.com>
2026-04-02 13:18:40 -0500

    add products and display order

--- app/components/orders/OrderSummary.tsx
+++ app/components/orders/OrderSummary.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+"use client";
+
+import { useStore } from "@/src/store";
+import ProductDetails from "./ProductDetails";
 
 export default function OrderSummary() {
+  const order = useStore((state) => state.order);
   return (
     <aside className="md:h-screen md:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">My Order</h1>
+
+      {!order.length ? (
+        <p className="text-center my-10">Shopping cart is empty.</p>
+      ) : (
+        <div className="mt-5">
+          {order.map((item) => (
+            <ProductDetails key={item.id} item={item} />
+          ))}
+        </div>
+      )}
     </aside>
   );
 }
--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@/src/generated/prisma/client";
 import { formatCurrency } from "@/src/utils";
 import Image from "next/image";
+import AddProductButton from "./AddProductButton";
 
 type ProductCardProps = {
   product: Product;
@@ -10,10 +11,11 @@ export default function ProductCard({ product }: ProductCardProps) {
   return (
     <div className="border bg-white">
       <Image
-        width={450}
-        height={500}
+        width={400}
+        height={400}
         src={`/products/${product.image}.jpg`}
         alt={`${product.name} album cover`}
+        loading="eager"
         className="my-0 mx-auto py-3"
       />
       <div className="p-5">
@@ -22,12 +24,7 @@ export default function ProductCard({ product }: ProductCardProps) {
         <p className="mt-5 font-black text-2xl text-amber-500">
           {formatCurrency(product.price)}
         </p>
-        <button
-          type="button"
-          className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
-        >
-          Add to Cart
-        </button>
+        <AddProductButton product={product} />
       </div>
     </div>
   );
