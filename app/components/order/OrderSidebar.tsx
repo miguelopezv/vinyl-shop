2a4b84e33ed0dd61a18f2ce6e20abadc3c211d89
Miguel López <miguelopezv@gmail.com>
2026-04-02 00:29:24 -0500

    retrieve categories from DB

--- app/components/order/OrderSidebar.tsx
+++ app/components/order/OrderSidebar.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import prisma from "@/src/lib/prisma";
 
-export default function OrderSidebar() {
+async function getCategories() {
+  return await prisma.category.findMany();
+}
+
+export default async function OrderSidebar() {
+  const categories = await getCategories();
+  console.log("🚀 ~ OrderSidebar ~ categories:", categories);
   return <aside className="md:w-72 md:h-screen bg-white">OrderSidebar</aside>;
 }
