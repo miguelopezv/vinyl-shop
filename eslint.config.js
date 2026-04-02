e9594806337a2c98245bd399382b9a597fbf85c3
Miguel López <miguelopezv@gmail.com>
2026-04-02 14:17:46 -0500

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,16 @@
 import next from "eslint-config-next";
+import { defineConfig } from "eslint/config";
+import nextPlugin from "@next/eslint-plugin-next";
 
-export default [
+export default defineConfig([
+  {
+    ignores: ["src/generated/**"],
+  },
   ...next,
   {
-    rules: {},
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
   },
-];
+]);
