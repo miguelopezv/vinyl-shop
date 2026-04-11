f0c260421e5569a325623004b381662383df5f00
Miguel López <miguelopezv@gmail.com>
2026-04-11 13:30:06 -0500

    handle error if server down

--- actions/login-admin-action.ts
+++ actions/login-admin-action.ts
@@ -6,17 +6,27 @@ import z from "zod";
 export default async function loginAdminAction(
   result: z.ZodSafeParseSuccess<AdminAuth>,
 ) {
-  const response = await fetch(
-    `${process.env.BACKEND_URL}${process.env.API_VERSION}/auth/login`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+  try {
+    const response = await fetch(
+      `${process.env.BACKEND_URL}${process.env.API_VERSION}/auth/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(result.data),
       },
-      body: JSON.stringify(result.data),
-    },
-  );
+    );
 
-  const data = await response.json();
-  return { data, ok: response.ok };
+    const data = await response.json();
+    return { data, ok: response.ok };
+  } catch (error) {
+    console.error("🚀 ~ loginAdminAction ~ error:", error);
+    return {
+      data: {
+        message: "Unable to connect to the server. Please try again later.",
+      },
+      ok: false,
+    };
+  }
 }
