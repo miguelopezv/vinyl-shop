ce34bdb073a0430405a1369c128da2b2e3b2e881
Miguel López <miguelopezv@gmail.com>
2026-04-11 14:01:16 -0500

    store token in cookie

--- actions/login-admin-action.ts
+++ actions/login-admin-action.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { AdminAuth } from "@/src/schemas";
+import { cookies } from "next/headers";
 import z from "zod";
 
 export default async function loginAdminAction(
@@ -19,9 +20,20 @@ export default async function loginAdminAction(
     );
 
     const data = await response.json();
+
+    if (response.ok) {
+      const cookieStore = await cookies();
+      cookieStore.set("VS_AUTH_TOKEN", data.token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+        path: "/",
+      });
+    }
+
     return { data, ok: response.ok };
   } catch (error) {
-    console.error("🚀 ~ loginAdminAction ~ error:", error);
     return {
       data: {
         message: "Unable to connect to the server. Please try again later.",
