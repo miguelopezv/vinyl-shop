e630757109992a0b1533483cdc74150386767473
Miguel López <miguelopezv@gmail.com>
2026-04-21 21:46:11 -0500

    ImageCard update

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { Product } from "@/src/generated/prisma/client";
-import { formatCurrency } from "@/src/utils";
+import { formatCurrency, getImagePath } from "@/src/utils";
 import Image from "next/image";
 import AddProductButton from "./AddProductButton";
 
@@ -13,10 +13,10 @@ export default function ProductCard({ product }: ProductCardProps) {
       <Image
         width={400}
         height={400}
-        src={`/products/${product.image}.jpg`}
+        src={getImagePath(product.image)}
         alt={`${product.name} album cover`}
         loading="eager"
-        className="my-0 mx-auto py-3"
+        className="my-0 mx-auto h-auto w-auto py-3"
       />
       <div className="p-5">
         <h3 className="text-2xl font-bold">{product.name}</h3>
