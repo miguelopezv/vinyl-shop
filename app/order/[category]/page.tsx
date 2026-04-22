17b524bbd5ae1774a5855f0817618c274555f9dc
Miguel López <miguelopezv@gmail.com>
2026-04-21 22:13:11 -0500

    fix styles

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -11,7 +11,7 @@ export default async function OrderPage({
   return (
     <>
       <Heading>Select your products</Heading>
-      <div className="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 gap-4 items-start">
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4 items-start">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
