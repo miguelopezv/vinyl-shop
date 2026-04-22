eb791f053c4bbb67ca47396b5c59444b1926f7b8
Miguel López <miguelopezv@gmail.com>
2026-04-21 21:44:51 -0500

    utils for slugs and imagePath

--- src/utils/formattters.ts
+++ src/utils/formattters.ts
@@ -1,6 +1,20 @@
-export function formatCurrency(amount: number) {
+export const formatCurrency = (amount: number) => {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
   }).format(amount);
-}
+};
+
+export const slugify = (value: string) =>
+  value
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9\s-]/g, "")
+    .replace(/\s+/g, "-");
+
+export const getImagePath = (imagePath: string) => {
+  const cloudinaryBaseUrl = "https://res.cloudinary.com";
+  return imagePath.startsWith(cloudinaryBaseUrl)
+    ? imagePath
+    : `/products/${imagePath}.jpg`;
+};
